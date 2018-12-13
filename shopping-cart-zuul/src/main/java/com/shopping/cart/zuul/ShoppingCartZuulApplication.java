d529c66fe82dc692b46d75f71fe9967f4d619513
chetanpatel2510 <chetanpatel2510@gmail.com>
2018-12-13 12:27:53 -0500

    Added eureka client

--- shopping-cart-zuul/src/main/java/com/shopping/cart/zuul/ShoppingCartZuulApplication.java
+++ shopping-cart-zuul/src/main/java/com/shopping/cart/zuul/ShoppingCartZuulApplication.java
@@ -2,8 +2,10 @@ package com.shopping.cart.zuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ShoppingCartZuulApplication {
 
 	public static void main(String[] args) {
