2e485e9adecf11f4f9705ad6e88053e840dc599c
chetanpatel2510 <chetanpatel2510@gmail.com>
2018-12-13 09:45:34 -0500

    First commit

---
+++ shopping-cart-eureka/src/main/java/com/shopping/cart/eureka/ShoppingCartEurekaApplication.java
@@ -0,0 +1,14 @@
+package com.shopping.cart.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingCartEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingCartEurekaApplication.class, args);
+	}
+
+}
+
