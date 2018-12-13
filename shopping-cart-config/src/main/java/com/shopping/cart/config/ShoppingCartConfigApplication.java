70904c06755b45889850f8f75f5c6cd9f1313a54
chetanpatel2510 <chetanpatel2510@gmail.com>
2018-12-13 12:52:02 -0500

    Initial Commit for Config Server

---
+++ shopping-cart-config/src/main/java/com/shopping/cart/config/ShoppingCartConfigApplication.java
@@ -0,0 +1,14 @@
+package com.shopping.cart.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingCartConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingCartConfigApplication.class, args);
+	}
+
+}
+
