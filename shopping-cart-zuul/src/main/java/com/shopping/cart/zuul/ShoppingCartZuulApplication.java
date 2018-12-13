591e73114bf7d473ce8c1ed8c9fdc92a4c8dc168
chetanpatel2510 <chetanpatel2510@gmail.com>
2018-12-13 11:04:48 -0500

    Initial Commit

---
+++ shopping-cart-zuul/src/main/java/com/shopping/cart/zuul/ShoppingCartZuulApplication.java
@@ -0,0 +1,14 @@
+package com.shopping.cart.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingCartZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingCartZuulApplication.class, args);
+	}
+
+}
+
