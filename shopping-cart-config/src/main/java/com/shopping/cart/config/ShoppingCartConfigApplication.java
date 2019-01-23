e2d520c5d06710c816661013014bf5e046dab092
chetanpatel2510 <chetanpatel2510@gmail.com>
2019-01-24 02:20:21 +0530

    Enable eureka client for config server

--- shopping-cart-config/src/main/java/com/shopping/cart/config/ShoppingCartConfigApplication.java
+++ shopping-cart-config/src/main/java/com/shopping/cart/config/ShoppingCartConfigApplication.java
@@ -3,9 +3,11 @@ package com.shopping.cart.config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableConfigServer
+@EnableEurekaClient
 public class ShoppingCartConfigApplication {
 
 	public static void main(String[] args) {
